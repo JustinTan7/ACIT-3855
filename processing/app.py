20dc7dd014e317c5d2ebf45070ab443eabb0112e
Justin Tan <Justin>
2023-11-21 08:54:04 -0800

    updated app.py in all services

--- processing/app.py
+++ processing/app.py
@@ -178,7 +178,8 @@ def get_stats():
 app = connexion.FlaskApp(__name__, specification_dir='')
 CORS(app.app)
 app.app.config['CORS_HEADERS'] = 'Content-Type'
-app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
+app.add_api("openapi.yml", base_path="/processing", strict_validation=True,
+validate_responses=True)
 
 if __name__ == "__main__":
     # run our standalone event server
